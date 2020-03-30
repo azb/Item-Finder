c5502c1eb27d9447ad70474eedbc229cb7919eb4
Arthur Z. Baney <azbaney@gmail.com>
2020-03-29 18:08:14 -0700

    Created script to show ARCore camera texture on a RawImage

---
+++ Assets/ShowARCameraRawImage.cs
@@ -0,0 +1,31 @@
+﻿using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowCamera : MonoBehaviour
+{
+
+    RawImage rawImage;
+
+    [SerializeField] private ARCoreBackgroundRenderer bgroundRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bgroundRenderer = FindObjectOfType<ARCoreBackgroundRenderer>();
+        rawImage = GetComponent<RawImage>();
+
+        rawImage.texture = bgroundRenderer.BackgroundMaterial.mainTexture;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rawImage.texture = bgroundRenderer.BackgroundMaterial.mainTexture;
+        }
+    }
+}
