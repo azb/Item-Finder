6e91217018b6a7848e265f3597b6a4b457f60921
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:04:39 -0700

    Made VRInputBox script to open vr keyboard when input box is clicked

---
+++ Assets/VRInputBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRInputBox : MonoBehaviour
+{
+    VRKeyboard keyboard;
+
+    VRButton vrButton;
+
+    InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = FindObjectOfType<VRKeyboard>();
+        vrButton = GetComponent<VRButton>();
+        vrButton.onClick.AddListener(OnClick);
+        inputField = GetComponent<InputField>();
+    }
+    
+    public void OnClick()
+    {
+        keyboard.Open(inputField);
+    }
+}
