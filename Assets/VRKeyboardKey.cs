3f32b43e2792d8db43a96c9ae9db332deddf7dd8
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:14:55 -0700

    Created VRKeyboardKey script for auto attachnig vr keys to the vr keyboard and entering correct character or command

---
+++ Assets/VRKeyboardKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRKeyboardKey : MonoBehaviour
+{
+    VRKeyboard keyboard;
+    
+    VRButton vrButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = FindObjectOfType<VRKeyboard>();
+        vrButton = GetComponent<VRButton>();
+        vrButton.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        if (keyboard == null)
+        {
+            keyboard = FindObjectOfType<VRKeyboard>();
+        }
+
+        keyboard.AddCharacter(gameObject);
+    }
+}
