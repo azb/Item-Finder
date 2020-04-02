bfef367186d0b6729fcdb345ec11a0b26143255c
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:10:15 -0700

    Made ItemMenuOpener script for opening the item menu on an item when it is clicked

---
+++ Assets/ItemMenuOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMenuOpener : MonoBehaviour
+{
+    ItemFinderUser user;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = FindObjectOfType<ItemFinderUser>();
+    }
+
+    public void OpenMenu()
+    {
+        GameObject menu = GameObject.FindWithTag("ItemOptions");
+        menu.GetComponent<ItemOptions>().Open(transform);
+    }
+}
