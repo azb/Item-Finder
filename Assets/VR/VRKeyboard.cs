15b80d5a0886356684d2a6a25444dae5a4da7d58
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:13:44 -0700

    Added open and close methods to vr keyboard, made enter functionality

--- Assets/VR/VRKeyboard.cs
+++ Assets/VR/VRKeyboard.cs
@@ -1,57 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class VRKeyboard : MonoBehaviour
 {
     public InputField inputField;
 
+    [SerializeField] private GameObject vrKeyboardCanvas;
+
+    private UnityEvent onHitEnterButton;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
+        onHitEnterButton = new UnityEvent();
     }
-
-    public void SetActiveInputField(InputField inputField)
-    {
-        Debug.Log("SetActiveInputField " + inputField.gameObject.name);
-
-        transform.parent.gameObject.SetActive(inputField != null);
-
-        this.inputField = inputField;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     public void AddCharacter(GameObject character)
     {
+
+
         if (inputField == null)
         {
-            inputField = FindObjectOfType<InputField>();
-        }
-
-        if (character.name == "BACKSPACE")
-        {
-            if (inputField.text.Length > 0)
-            {
-                inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
-            }
+            Debug.LogError("No input field set for vr keyboard");
         }
         else
         {
-            inputField.text += character.name;
+            if (character.name == "BACKSPACE")
+            {
+                if (inputField.text.Length > 0)
+                {
+                    inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+                }
+            }
+            else
+            if (character.name == "ENTER")
+            {
+                if (onHitEnterButton != null)
+                {
+                    onHitEnterButton.Invoke();
+                }
+            }
+            else
+            {
+                inputField.text += character.name;
+            }
         }
+    }
+
+    public void SetEnterAction(UnityAction onHitEnterButton)
+    {
+        this.onHitEnterButton.AddListener(onHitEnterButton);
     }
 
     public void Close()
     {
-        transform.parent.gameObject.SetActive(false);
+        vrKeyboardCanvas.SetActive(false);
     }
 
-
+    public void Open(InputField inputField)
+    {
+        vrKeyboardCanvas.SetActive(true);
+        this.inputField = inputField;
+    }
 }
