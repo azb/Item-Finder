d684702943d4ac73aaf43e886ca3c26dd5c8bb6a
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:06:21 -0700

    Made ItemOptions script for opening item options menu when an item is clicked on

---
+++ Assets/ItemOptions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOptions : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+
+    ItemFinderUser user;
+
+    ItemGameObject selectedItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = FindObjectOfType<ItemFinderUser>();
+    }
+
+    public void Open(Transform item)
+    {
+        selectedItem = item.GetComponent<ItemGameObject>();
+        
+        if (user.state == ItemFinderUser.State.None)
+        {
+            panel.gameObject.SetActive(true);
+
+            GameObject mainCamera = GameObject.FindWithTag("MainCamera");
+
+            transform.position = (item.position + mainCamera.transform.position) / 2f;
+        }
+    }
+
+    public void Close()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    public void RenameItem(string newName)
+    {
+        selectedItem.Rename(newName); 
+    }
+}
