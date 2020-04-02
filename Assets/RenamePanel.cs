182472859e98702681a3010bb51b40ade0e073f2
Arthur Z. Baney <azbaney@gmail.com>
2020-04-02 03:11:22 -0700

    Made RenamePanel script for handling when an object is renamed

---
+++ Assets/RenamePanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenamePanel : MonoBehaviour
+{
+    [SerializeField] private ItemOptions itemOptions;
+    
+    VRKeyboard keyboard;
+    
+    void OnEnable()
+    {
+        keyboard = FindObjectOfType<VRKeyboard>();   
+        keyboard.SetEnterAction(DoRename);
+    }
+    
+    public void DoRename()
+    {
+        itemOptions.RenameItem(keyboard.inputField.text);
+        keyboard.Close();
+        itemOptions.Close();
+    }
+}
