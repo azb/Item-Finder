f68bafca2d4805097b10b17f97b1fc01bf4b008b
Arthur Z. Baney <azbaney@gmail.com>
2020-04-04 12:32:45 -0700

    Add GameObjectToggler for switching between gameobjects

---
+++ Assets/GameObjectToggler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectToggler : MonoBehaviour
+{
+    public GameObject[] gameObjectsToToggle;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("GetsHere10 "+gameObject.name);
+
+
+    }
+
+    public void Toggle(GameObject gameObjectToEnable)
+    {
+        for (int i = 0; i < gameObjectsToToggle.Length; i++)
+        {
+            gameObjectsToToggle[i].SetActive( gameObjectsToToggle[i] == gameObjectToEnable );
+        }
+    }
+}
