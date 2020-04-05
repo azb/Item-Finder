1670fbd277bc63fb79762965c1025831088efd43
Arthur Z. Baney <azbaney@gmail.com>
2020-04-04 22:48:43 -0700

    Made file browser scrollable with vr controllers thumbsticks

---
+++ Assets/OVRScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OVRScroll : MonoBehaviour
+{
+    [SerializeField] private Scrollbar scrollbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scrollbar.value += OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y / 10f;
+        scrollbar.value += OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y / 10f;
+    }
+}
