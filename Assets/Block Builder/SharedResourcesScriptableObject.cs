023a44f399bef86139681b422e884dcab756a89d
Arthur Z. Baney <azbaney@gmail.com>
2020-04-05 11:27:53 -0700

    Added functionality to close VRKeyboard to shared resource

--- Assets/Block Builder/SharedResourcesScriptableObject.cs
+++ Assets/Block Builder/SharedResourcesScriptableObject.cs
@@ -9,12 +9,14 @@ public class SharedResourcesScriptableObject : ScriptableObject
     public GameObject itemOptionsStartPanel;
     public ItemGameObject selectedItem;
 
+    VRKeyboard vrKeyboard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    
+
     public void OpenItemOptionsTab()
     {
         vruiTabGroup.gameObject.SetActive(true);
@@ -24,9 +26,23 @@ public class SharedResourcesScriptableObject : ScriptableObject
     public void CloseUI()
     {
         vruiTabGroup.gameObject.SetActive(false);
+        CloseVRKeyboard();
     }
 
+    public void CloseVRKeyboard()
+    {
+        if (vrKeyboard != null)
+        {
+            vrKeyboard.Close();
+        }
+        else
+        {
+            Debug.LogError("VR keyboard not set");
+        }
+    }
 
-
-
+    public void SetVRKeyboard(VRKeyboard vrKeyboard)
+    {
+        this.vrKeyboard = vrKeyboard;
+    }
 }
